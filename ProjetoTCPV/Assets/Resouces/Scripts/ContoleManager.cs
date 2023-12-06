6a916668d8377863651efdf770c2d2fa030d7790
Joao-M-Bastos <jmb32180@gmail.com>
2023-12-06 08:50:50 -0300

    menu

---
+++ ProjetoTCPV/Assets/Resouces/Scripts/ContoleManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContoleManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("0_MenuInicial"); 
+
+    }
+}
